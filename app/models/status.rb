457cb14b33fa05cfdc05713de6b6195c7b2c1fb7
Esther S Weon <esther.s.weon@gmail.com>
2014-05-09 12:34:20 -0700

    twitter

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,7 +2,7 @@ require 'launchy'
 require 'json'
 require 'addressable/uri'
 
-class Status # < ActiveRecord::Base
+class Status < ActiveRecord::Base
   # validates :text, :twitter_status_id, :twitter_user_id, :presence => true
 
   def self.fetch_by_twitter_user_id!(user_id)
@@ -10,13 +10,20 @@ class Status # < ActiveRecord::Base
   end
 
   def self.parse_json(fetched_timeline)
-    txt = fetched_timeline[0]["text"]
-    user_id = fetched_timeline[0]["user"]["id"]
-    status_id = fetched_timeline[0]["id_str"]
 
-    Status.new(txt, status_id, user_id)
+    fetched_timeline.each do |status|
+      txt = status["text"]
+      user_id = status["user"]["id"]
+      status_id = status["id_str"]
+
+      p Status.new(txt, status_id, user_id).save!
+    end
   end
 
-
+  def initialize(txt, status_id, user_id)
+    @txt = txt
+    @status_id = status_id
+    @user_id = user_id
+  end
 
 end
